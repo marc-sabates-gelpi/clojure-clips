f9e1217b32779e6f8963b77ff37f6bf40020f3b3
Marc Sabates Gelpi <marc.sabates.gelpi.workspace@gmail.com>
2018-02-01 19:06:34 +0000

    Checkpoint 01/02/2018

--- src/clips/core.clj
+++ src/clips/core.clj
@@ -39,7 +39,63 @@
 (defn follow-pipe [pipes]
   (loop [dir DOWN current-pos [0 (find-pipes-beginning pipes)] collected-letters []]
     (if (= dir :end)
-      collected-letters
+      {:letters collected-letters :steps 0}
       (let [{:keys [pos collection]} (get-straight-pipe pipes current-pos (partial move dir))]
         (recur (find-next-direction pipes pos dir) pos (into collected-letters (comp (filter letter?)) collection))))))
 (follow-pipe [[\| \space \F] [\| \space \|] [\+ \- \+]])
+;;01/02/2018
+(-> "tubes"
+    slurp
+    clojure.string/split-lines
+    (as-> rows (sequence (comp
+                          (map sequence)
+                          (map vec))
+                         rows))
+    vec
+    follow-pipe
+    (as-> result (reduce str (:letters result))))
+;; -- Part 2
+(defn count-chars []
+  (-> "tubes"
+   slurp
+;;    "     |          
+;;      |  +--+    
+;;      A  |  C    
+;;  F---|--|-E---+ 
+;;      |  |  |  D 
+;;      +B-+  +--+ 
+;; "
+   (as-> input (clojure.string/replace input #"\s" ""))
+   count))
+(defn crossing? [pipes [r c]]
+  (let [u (get-in pipes [r (dec c)] \space)
+        d (get-in pipes [r (inc c)] \space)
+        l (get-in pipes [(dec r) c] \space)
+        r (get-in pipes [(inc r) c] \space)]
+    (and (not= \space u)
+         (not= \space d)
+         (not= \space l)
+         (not= \space r))))
+(defn num-crossings [pipes]
+  (for [r (range (count pipes)) c (range (count (first pipes)))]
+    (crossing? pipes [r c])))
+(defn count-crossings []
+  (-> "tubes"
+   slurp
+;;    "     |          
+;;      |  +--+    
+;;      A  |  C    
+;;  F---|--|-E---+ 
+;;      |  |  |  D 
+;;      +B-+  +--+ 
+;; "
+   clojure.string/split-lines
+   (as-> rows (sequence (comp
+                         (map sequence)
+                         (map vec))
+                        rows))
+   vec
+   num-crossings
+   frequencies
+   (get true)))
+(prn (+ (count-chars) (count-crossings)))
