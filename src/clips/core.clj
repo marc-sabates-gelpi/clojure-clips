98b4dbb0f22c6a64ffff06fe3aef1dbedc8b6e4b
Marc Sabates Gelpi <marc.sabates.gelpi@protonmail.com>
2018-03-01 20:25:12 +0000

    Session 1/03/2018

--- src/clips/core.clj
+++ src/clips/core.clj
@@ -52,3 +52,45 @@ dorun
 (= nil (analyze [[:x :o :x]
                  [:x :o :x]
                  [:o :x :o]]))
+;;#74 Filter Perfect Squares
+;; C-c RET h d [org.clojure/math.numeric-tower "0.0.4"]
+(require '[clojure.math.numeric-tower :as math])
+(def mem-sqrt (memoize math/sqrt))
+(defn perf-sq [s]
+  (as-> (clojure.string/split s #",") nums
+    (transduce (comp
+                (map read-string)
+                (filter #(int? (mem-sqrt %)))
+                (map str)
+                (interpose ","))
+               (completing str)
+               nums)))
+(int? (math/sqrt 2))
+(str)
+
+(= (perf-sq "4,5,6,7,8,9") "4,9")
+(= (perf-sq "15,16,25,36,37") "16,25,36")
+;; v1.4 compatible
+(defn perf-sq [s]
+  (-> (clojure.string/split s #",")
+      ((fn [nums] (map read-string nums)))
+      ((fn [nums] (filter #(let [sqrt (mem-sqrt %)] (= sqrt (int sqrt))) nums)))
+      ((fn [nums] (map str nums)))
+      ((fn [nums] (interpose "," nums)))
+      ((fn [nums] (reduce str nums)))))
+(= (perf-sq "4,5,6,7,8,9") "4,9")
+(= (perf-sq "15,16,25,36,37") "16,25,36")
+(def perf-sq (let [mem-perf-sq? (memoize (fn [n]
+                                           (let [initial-candidates (for [x (range 1 n) :when (>= n (* x x))] x)]
+                                             (loop [candidates initial-candidates found? false]
+                                               (if (empty? candidates)
+                                                 found?
+                                                 (recur (rest candidates) (let [cand (first candidates)]
+                                                                            (= n (* cand cand)))))))))]
+               (fn [s]
+                 (-> (clojure.string/split s #",")
+                     ((fn [nums] (map read-string nums)))
+                     ((fn [nums] (filter mem-perf-sq? nums)))
+                     ((fn [nums] (map str nums)))
+                     ((fn [nums] (interpose "," nums)))
+                     ((fn [nums] (reduce str nums)))))))
