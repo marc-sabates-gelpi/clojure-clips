6134d3f9937b8139326b1a782da03ef834df2569
Marc Sabates Gelpi <marc.sabates.gelpi@protonmail.com>
2018-03-25 17:27:59 +0100

    AoC 2016 Day 6 Part 2

--- src/clips/core.clj
+++ src/clips/core.clj
@@ -999,3 +999,32 @@ enarar"
   (comment (println "I am commented out, too.."))
   #_ (println "I am commented out, as well..")
   #break (+ 2 2))
+
+;;; AoC 2016 Day 6 Part 2
+
+(->> "resources/aoc2016/message"
+     slurp
+     #_"eedadn
+drvtee
+eandsr
+raavrd
+atevrs
+tsrnev
+sdttsa
+rasrtv
+nssdts
+ntnada
+svetve
+tesnvt
+vntsnd
+vrdear
+dvrsen
+enarar" 
+     clojure.string/split-lines
+     (reduce #(apply mapv conj %&) (for [x (range #_6 8)] []))
+     (sequence (comp
+                (map frequencies)
+                (map #(sort-by val %))
+                (map first)
+                (map key)))
+     (apply str))
