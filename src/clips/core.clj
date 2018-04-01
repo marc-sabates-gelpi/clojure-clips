4b95ad40b50a8dffa7cf15345b91b449b4b4fc40
Marc Sabates Gelpi <marc.sabates.gelpi@protonmail.com>
2018-04-01 12:54:03 +0100

    Session 01/04/2018

--- src/clips/core.clj
+++ src/clips/core.clj
@@ -1,3 +1,24 @@
 (ns clips.core
   (:require [clojure.core.async :as async]
             [spyscope.core]))
+;;; Session 01/04/2018
+
+(reduce + 0 (range 10))
+
+(def ^:private some-primes '(3 5 7 11 13 17 19 23))
+
+(defn- my-processor-f
+  "Function that processes an element to a collection."
+  [x]
+  (when (< x 5)
+    (map #(* % x) some-primes)))
+
+(defn- my-reduce-f
+  "Function that reduces to itself."
+  [acc el]
+  (printf "Accumulated: %s. Current element: %s\n" acc el)
+  (reduce my-reduce-f (conj acc el) (my-processor-f el)))
+
+(reduce my-reduce-f #{} (my-processor-f 1))
+
+
