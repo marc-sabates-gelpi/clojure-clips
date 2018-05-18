5cd3b01e713e067b4c2c4a13dd2cf37e3dc576cc
Marc Sabates Gelpi <marc.sabates.gelpi@protonmail.com>
2018-05-19 00:25:30 +0100

    AOC2016 Day 8 Part 1

--- src/clips/core.clj
+++ src/clips/core.clj
@@ -39,36 +39,70 @@
   "Parse a screen instruction.
   Try to match `pattern` and if so return screen op `op`,
   otherwsie return identity."
-  [pattern op s]
-  (if-let [[_ a b] (and (string? s) (re-find pattern s))]
+  [pattern op instr]
+  (if-let [[_ a b] (and (string? instr) (re-find pattern instr))]
     {:op op :a (read-string a) :b (read-string b)}
-    s))
+    instr))
 
 (defn- parse-rect
   "Parse a rect instruction or return identity.
   The instruction follows this pattern: `rect AxB`."
-  [s]
-  (parse-screen-instr #"\brect\s+([0-9]+)x([0-9]+)\b" :rec s))
+  [instr]
+  (as-> (parse-screen-instr #"\brect\s+([0-9]+)x([0-9]+)\b" :rec instr) parsed
+    (assoc parsed :wide (get parsed :a))
+    (assoc parsed :tall (get parsed :b))
+    (dissoc parsed :a)
+    (dissoc parsed :b)))
 
 (defn- parse-rot-row
   "Parse a rotate row instruction or return identity.
   The instruction follows this pattern: `rotate row y=A by B`."
-  [s]
-  (parse-screen-instr #"\brotate\s+row\s+y=([0-9]+)\s+by\s+([0-9]+)\b" :rot-row s))
+  [instr]
+  (as-> (parse-screen-instr #"\brotate\s+row\s+y=([0-9]+)\s+by\s+([0-9]+)\b" :rot-row instr) parsed
+    (assoc parsed :row (get parsed :a))
+    (assoc parsed :by (get parsed :b))
+    (dissoc parsed :a)
+    (dissoc parsed :b)))
 
 (defn- parse-rot-col
   "Parse a rotate col instruction or return identity.
   The instruction follows this pattern: `rotate column x=A by B`."
-  [s]
-  (parse-screen-instr #"\brotate\s+column\s+x=([0-9]+)\s+by\s+([0-9]+)\b" :rot-col s))
+  [instr]
+  (as-> (parse-screen-instr #"\brotate\s+column\s+x=([0-9]+)\s+by\s+([0-9]+)\b" :rot-col instr) parsed
+    (assoc parsed :col (get parsed :a))
+    (assoc parsed :by (get parsed :b))
+    (dissoc parsed :a)
+    (dissoc parsed :b)))
+
+(defmulti run-screen-instr
+  "Execute a screen instruction."
+  (fn [_ parsed] (:op parsed)))
+
+(defmethod run-screen-instr :rec
+  [screen {:keys [wide tall]}]
+  (let [positions (for [col (range wide) row (range tall)]
+                    [row col])]
+    (reduce #(assoc-in %1 %2 \#) screen positions)))
+
+(defmethod run-screen-instr :rot-row
+  [screen {:keys [row by]}]
+  (assoc screen row (->> (get screen row)
+                         (repeat 2)
+                         flatten
+                         (drop (- wide by))
+                         (take wide)
+                         vec)))
+
+(defmethod run-screen-instr :rot-col
+  [screen {:keys [col by]}])
 
 (-> "resources/aoc2016/screen-instructions"
     slurp
     clojure.string/split-lines
-    (as-> lines (sequence #_transduce (comp
+    (as-> lines (#_sequence transduce (comp
                                        (map parse-rect)
                                        (map parse-rot-row)
                                        (map parse-rot-col))
-                           #_(completing run-screen-instr)
-                           #_(empty-screen wide tall)
+                           (completing run-screen-instr)
+                           (empty-screen wide tall)
                            lines)))
