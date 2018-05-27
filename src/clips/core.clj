37d4cc19e807b6026fe56bd944b4e3b581eb0322
Marc Sabates Gelpi <marc.sabates.gelpi@protonmail.com>
2018-05-27 19:06:14 +0100

    More alternatives to search for a value on a map

--- src/clips/core.clj
+++ src/clips/core.clj
@@ -163,3 +163,62 @@
    (->> responses
         (some-in application.response/greenhouse-name name-looked-for)
         :application.response/answer)])
+
+;;; Session 27/05/2018
+
+;; `some-in` could follow the HOF approach
+;; using a `pred` instead of `k` `v`.
+
+(defmacro some-full-map [pred coll]
+  "`some` in `coll` returning the full map.
+  `pred` provides selection."
+  `(some
+    (fn [m#]
+      (when (~pred m#)
+        m#))
+    ~coll))
+
+;; refactor
+
+(defmacro some-in [k v coll]
+  "`some` val `v` in `coll` of maps with key `k`."
+  `(some
+    (fn [{:keys [~k] :as whole#}]
+      (when (#{~v} ~(symbol (.getName k)))
+        whole#))
+    ~coll))
+
+(let [name-looked-for :name2
+      responses '({:application.response/greenhouse-name :name0 :application.response/answer "Answer 0"} {:application.response/greenhouse-name :name1 :application.response/answer "Answer 1"} {:application.response/greenhouse-name :name2 :application.response/answer "Answer 2"})]
+  [;; original
+   (->> responses
+         (filter #(= name-looked-for (:application.response/greenhouse-name %)))
+         first
+         :application.response/answer)
+
+   ;; alternative using `some`
+   (->> responses
+        (some (fn [{:keys [application.response/greenhouse-name] :as whole}]
+                (when (some #{name-looked-for} (list greenhouse-name))
+                  whole)))
+        :application.response/answer)
+
+   ;; alternative using custom `some-in` macro
+   (->> responses
+        (some-in application.response/greenhouse-name name-looked-for)
+        :application.response/answer)
+
+   ;; alternative using custom `some-full-map` macro
+   (->> responses
+        (some-full-map (comp
+                        #{name-looked-for}
+                        :application.response/greenhouse-name))
+        :application.response/answer)
+
+   ;; alternative using `#{}`
+   (->> responses
+        (filter (comp
+                 #{name-looked-for}
+                 :application.response/greenhouse-name))
+        first
+        :application.response/answer)])
