d80f6a0e5b9ebdd5f7d69eb3fce382c0cb073f9e
Marc Sabates Gelpi <marc.sabates.gelpi@protonmail.com>
2018-05-27 02:15:09 +0100

    Create macro some-in

--- src/clips/core.clj
+++ src/clips/core.clj
@@ -123,3 +123,43 @@
     surname (assoc :surname surname)
     tel (assoc :tel tel)
     true (assoc :timestamp (java.util.Date.))))
+
+;;; Idiomatic clojure
+
+(let [k :name0]
+  (some (comp #{k} :field1) '({:field1 :name1 :field2 :value1} {:field1 :name0 :field2 :value0})))
+
+(let [k :name0]
+  (some (fn [{:keys [field1] :as whole}] (when (some #{k} (list field1)) whole)) '({:field1 :name1 :field2 :value1} {:field1 :name0 :field2 :value0})))
+
+(let [k :name0]
+  (some (fn [{:keys [nspace/field1] :as whole}] (when (some #{k} (list field1)) whole)) '({:nspace/field1 :name1 :nspace/field2 :value1} {:nspace/field1 :name0 :nspace/field2 :value0})))
+
+(defmacro some-in [k v coll]
+  "`some` val `v` in `coll` of maps with key `k`."
+  `(some (fn [{:keys [~k] :as whole#}]
+           (when (some #{~v} (list ~(symbol (.getName k))))
+                  whole#)) ~coll))
+
+(let [k :name0]
+  (some-in field1 k '({:field1 :name1 :field2 :value1} {:field1 :name0 :field2 :value0})))
+
+(let [k :name0]
+  (some-in nspace/field1 k '({:nspace/field1 :name1 :nspace/field2 :value1} {:nspace/field1 :name0 :nspace/field2 :value0})))
+
+(let [name-looked-for :name2
+      responses '({:application.response/greenhouse-name :name0 :application.response/answer "Answer 0"} {:application.response/greenhouse-name :name1 :application.response/answer "Answer 1"} {:application.response/greenhouse-name :name2 :application.response/answer "Answer 2"})]
+  [(->> responses
+         (filter #(= name-looked-for (:application.response/greenhouse-name %)))
+         first
+         :application.response/answer)
+
+   (->> responses
+        (some (fn [{:keys [application.response/greenhouse-name] :as whole}]
+                (when (some #{name-looked-for} (list greenhouse-name))
+                  whole)))
+        :application.response/answer)
+
+   (->> responses
+        (some-in application.response/greenhouse-name name-looked-for)
+        :application.response/answer)])
