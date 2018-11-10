305e6a73e65516d3b4ca3be2ccc3857c6321dc7b
Marc Sabates Gelpi <marc.sabates.gelpi@protonmail.com>
2018-11-10 18:31:46 +0000

    Archive & AoC 2016 Day 9 Part 1

---
+++ src/clips/201807-10.clj
@@ -0,0 +1,89 @@
+(ns clips.core
+  (:require [clojure.core.async :as async]
+            [spyscope.core]
+            [clj-memory-meter.core :as mm]))
+
+;;;; Session 03/08/2018
+
+(->> '({:id 1 :val 1} {:id 2 :val 2})
+     (group-by :id)
+     (reduce (fn [acc [k v]] (assoc acc k (first v))) {}))
+
+;;; Challenge: Read a file as lazily as possible
+
+;; Read chars 1 at a time
+(import '(java.io FileInputStream))
+
+(defn read-char
+  "Read a single char from `InputStream`."
+  [is]
+  (let [c (.read is)]
+    (if-not (neg? c)
+      (str (char c))
+      (.close is))))
+
+(with-open [fis (FileInputStream. "resources/aoc2016/day9")]
+  (let [fst-char (read-char fis)
+        scnd-char (read-char fis)]
+    (print (format "Read %d char: %s" (count fst-char) fst-char)
+           (format "Read %d char: %s" (count scnd-char) scnd-char))))
+
+;; Create a lazy seq of chars (as strings)
+(defn lazy-file
+  "Return a lazy seq from the input stream `is`."
+  [is]
+  (when-let [c (read-char is)]
+    (lazy-seq (cons c (lazy-file is)))))
+
+(defn read-file
+  "Make file reader lazy-list."
+  [file-path]
+  (lazy-file (FileInputStream. file-path)))
+
+(def f1 (read-file "resources/aoc2016/one"))
+(def day9-file (read-file "resources/aoc2016/day9"))
+
+(take 3 f1)
+(reduce str (take 2 day9-file))
+
+;;; Memory measurements
+#_(with-open [rdr (clojure.java.io/reader "resources/aoc2016/day9")]
+  (mm/measure rdr)) ; => 32.8 KB
+
+#_(mm/measure f1) ; => 272 B
+#_(mm/measure day9-file) ; => 272 B
+#_(take 50 day9-file)
+#_(mm/measure day9-file) ; => 5.7 KB
+#_(count day9-file) ; => 14248
+#_(mm/measure day9-file) ; => 1.5 MB
+
+#_(mm/measure (slurp "resources/aoc2016/day9")) ; => 27.9 KB
+#_(count (slurp "resources/aoc2016/day9"))
+
+(mm/measure (read-file "resources/aoc2016/day9")) ; => 272 B
+(mm/measure (take 50 (read-file "resources/aoc2016/day9"))) ; => 352 B
+(mm/measure (reduce str (read-file "resources/aoc2016/day9"))) ; => 27.9 KB
+(let [content (reduce str (read-file "resources/aoc2016/day9"))]
+  (print content)
+  (mm/measure content)) ; => 27.9 B
+;; Clearly 27.9 B vs 1.5 MB is the difference between holding onto the
+;; head of a lazy seq or not!
+
+(require '[cemerick.url :refer (url url-encode)])
+(-> (url "https://www.host.domain/path" "path2" "path3")
+    (assoc-in [:query :session-id] "session-id with=encodable&characters")
+    str)
+
+(-> (url "https://www.host.domain/path" "path2" "path3")
+    (assoc :query {"session-id" "session-id with=encodable&characters"})
+    str)
+
+(= *1 *2) ; => true
+
+(let [a (atom {:a [{:b :valb :c :valc}]})]
+  (swap! a update-in [:a 0] dissoc :b)
+  @a)
+
+(into {} '(["session-id" {:value "session-id-1"}] ["session-id" {:value "session-id-2"}])) ; => {"session-id" {:value "session-id-2"}}
+
+(into {} '(["session-id" {:value "session-id-1"}] ["session-id" {:value "session-id-2"}] ["ring-id" {:value "ring-id"}])) ; => {"session-id" {:value "session-id-2"} "rind-id" {:value "ring-id"}}
--- src/clips/core.clj
+++ src/clips/core.clj
@@ -1,84 +1,40 @@
 (ns clips.core
   (:require [clojure.core.async :as async]
             [spyscope.core]
-            [clj-memory-meter.core :as mm]))
-
-;;;; Session 03/08/2018
-
-(->> '({:id 1 :val 1} {:id 2 :val 2})
-     (group-by :id)
-     (reduce (fn [acc [k v]] (assoc acc k (first v))) {}))
-
-;;; Challenge: Read a file as lazily as possible
-
-;; Read chars 1 at a time
-(import '(java.io FileInputStream))
-
-(defn read-char
-  "Read a single char from `InputStream`."
-  [is]
-  (let [c (.read is)]
-    (if-not (neg? c)
-      (str (char c))
-      (.close is))))
-
-(with-open [fis (FileInputStream. "resources/aoc2016/day9")]
-  (let [fst-char (read-char fis)
-        scnd-char (read-char fis)]
-    (print (format "Read %d char: %s" (count fst-char) fst-char)
-           (format "Read %d char: %s" (count scnd-char) scnd-char))))
-
-;; Create a lazy seq of chars (as strings)
-(defn lazy-file
-  "Return a lazy seq from the input stream `is`."
-  [is]
-  (when-let [c (read-char is)]
-    (lazy-seq (cons c (lazy-file is)))))
-
-(defn read-file
-  "Make file reader lazy-list."
-  [file-path]
-  (lazy-file (FileInputStream. file-path)))
-
-(def f1 (read-file "resources/aoc2016/one"))
-(def day9-file (read-file "resources/aoc2016/day9"))
-
-(take 3 f1)
-(reduce str (take 2 day9-file))
-
-;;; Memory measurements
-#_(with-open [rdr (clojure.java.io/reader "resources/aoc2016/day9")]
-  (mm/measure rdr)) ; => 32.8 KB
-
-#_(mm/measure f1) ; => 272 B
-#_(mm/measure day9-file) ; => 272 B
-#_(take 50 day9-file)
-#_(mm/measure day9-file) ; => 5.7 KB
-#_(count day9-file) ; => 14248
-#_(mm/measure day9-file) ; => 1.5 MB
-
-#_(mm/measure (slurp "resources/aoc2016/day9")) ; => 27.9 KB
-#_(count (slurp "resources/aoc2016/day9"))
-
-(mm/measure (read-file "resources/aoc2016/day9")) ; => 272 B
-(mm/measure (take 50 (read-file "resources/aoc2016/day9"))) ; => 352 B
-(mm/measure (reduce str (read-file "resources/aoc2016/day9"))) ; => 27.9 KB
-(let [content (reduce str (read-file "resources/aoc2016/day9"))]
-  (print content)
-  (mm/measure content)) ; => 27.9 B
-;; Clearly 27.9 B vs 1.5 MB is the difference between holding onto the
-;; head of a lazy seq or not!
-
-(require '[cemerick.url :refer (url url-encode)])
-(-> (url "https://www.host.domain/path" "path2" "path3")
-    (assoc-in [:query :session-id] "session-id with=encodable&characters")
-    str)
-
-(-> (url "https://www.host.domain/path" "path2" "path3")
-    (assoc :query {"session-id" "session-id with=encodable&characters"})
-    str)
-
-(= *1 *2) ; => true
+            [clj-memory-meter.core :as mm]
+            [clojure.string :as string]))
 
 ;;; AoC 2016 Day 9 Part 1
-(reduce (comp ) {:} (read-file "resources/aoc2016/day9"))
+(defn uncompress
+  [in]
+  (if-not (= \( (first in))
+    [(subs in 1 (count in)) (subs in 0 1)]
+    (let [cmd (re-find #"\([0-9]+[Xx][0-9]+\)" in)
+          [len times] (map clojure.edn/read-string (-> cmd
+                                                       (string/replace #"[\(\)]" "")
+                                                       (string/split #"[Xx]")))
+          remaining-in (subs in (+ (count cmd) len) (count in))
+          pattern (subs in (count cmd) (+ (count cmd) len))
+          out (apply str (take (* len times) (cycle pattern)))]
+      [remaining-in out])))
+
+(defn day9-1
+  [in]
+  (let [input (if (string/index-of in "resources/") (slurp in) in)]
+    (loop [compressed input uncompressed nil]
+      (if (seq compressed)
+        (let [[updated-compressed new-chunk] (uncompress compressed)]
+          (recur updated-compressed (str uncompressed new-chunk)))
+        uncompressed))))
+
+(defmacro str-and-count
+  [form#]
+  `((juxt identity count) ~form#))
+
+(str-and-count (day9-1 "ADVENT"))
+(str-and-count (day9-1 "A(1x5)BC"))
+(str-and-count (day9-1 "(3x3)XYZ"))
+(str-and-count (day9-1 "A(2x2)BCD(2x2)EFG"))
+(str-and-count (day9-1 "(6x1)(1x3)A"))
+(str-and-count (day9-1 "X(8x2)(3x3)ABCY"))
+(str-and-count (day9-1 "resources/aoc2016/day9"))
