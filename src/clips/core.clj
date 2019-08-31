b877c28b7c1c85e0bdd6e01ee9ac1167e906c03c
Marc Sabates Gelpi <marc.sabates.gelpi@protonmail.com>
2019-08-31 17:04:38 +0100

    if-let-xf macro

--- src/clips/core.clj
+++ src/clips/core.clj
@@ -54,45 +54,109 @@ clojure.core/default-data-readers;; => {uuid #'clojure.uuid/default-uuid-reader,
 (read-string "#=(+ 1 1)");; => 2
 (binding [*read-eval* false] (read-string "#=(+ 1 1)"))
 *e;; => #error {
- :cause "EvalReader not allowed when *read-eval* is false."
- :via
- [{:type java.lang.RuntimeException
-   :message "EvalReader not allowed when *read-eval* is false."
-   :at [clojure.lang.Util runtimeException "Util.java" 221]}]
- :trace
- [[clojure.lang.Util runtimeException "Util.java" 221]
-  [clojure.lang.LispReader$EvalReader invoke "LispReader.java" 1297]
-  [clojure.lang.LispReader$DispatchReader invoke "LispReader.java" 853]
-  [clojure.lang.LispReader read "LispReader.java" 285]
-  [clojure.lang.LispReader read "LispReader.java" 216]
-  [clojure.lang.LispReader read "LispReader.java" 205]
-  [clojure.lang.RT readString "RT.java" 1874]
-  [clojure.lang.RT readString "RT.java" 1869]
-  [clojure.core$read_string invokeStatic "core.clj" 3815]
-  [clojure.core$read_string invoke "core.clj" 3805]
-  [clips.core$eval13448 invokeStatic "NO_SOURCE_FILE" 55]
-  [clips.core$eval13448 invoke "NO_SOURCE_FILE" 55]
-  [clojure.lang.Compiler eval "Compiler.java" 7176]
-  [clojure.lang.Compiler eval "Compiler.java" 7131]
-  [clojure.core$eval invokeStatic "core.clj" 3214]
-  [clojure.core$eval invoke "core.clj" 3210]
-  [clojure.main$repl$read_eval_print__9068$fn__9071 invoke "main.clj" 414]
-  [clojure.main$repl$read_eval_print__9068 invoke "main.clj" 414]
-  [clojure.main$repl$fn__9077 invoke "main.clj" 435]
-  [clojure.main$repl invokeStatic "main.clj" 435]
-  [clojure.main$repl doInvoke "main.clj" 345]
-  [clojure.lang.RestFn applyTo "RestFn.java" 137]
-  [clojure.core$apply invokeStatic "core.clj" 665]
-  [clojure.core$apply invoke "core.clj" 660]
-  [refactor_nrepl.ns.slam.hound.regrow$wrap_clojure_repl$fn__9642 doInvoke "regrow.clj" 18]
-  [clojure.lang.RestFn invoke "RestFn.java" 1523]
-  [nrepl.middleware.interruptible_eval$evaluate invokeStatic "interruptible_eval.clj" 79]
-  [nrepl.middleware.interruptible_eval$evaluate invoke "interruptible_eval.clj" 55]
-  [nrepl.middleware.interruptible_eval$interruptible_eval$fn__935$fn__939 invoke "interruptible_eval.clj" 142]
-  [clojure.lang.AFn run "AFn.java" 22]
-  [nrepl.middleware.session$session_exec$main_loop__1036$fn__1040 invoke "session.clj" 171]
-  [nrepl.middleware.session$session_exec$main_loop__1036 invoke "session.clj" 170]
-  [clojure.lang.AFn run "AFn.java" 22]
-  [java.lang.Thread run "Thread.java" 748]]}
+ ;; :cause "EvalReader not allowed when *read-eval* is false."
+ ;; :via
+ ;; [{:type java.lang.RuntimeException
+ ;;   :message "EvalReader not allowed when *read-eval* is false."
+ ;;   :at [clojure.lang.Util runtimeException "Util.java" 221]}]
+ ;; :trace
+ ;; [[clojure.lang.Util runtimeException "Util.java" 221]
+ ;;  [clojure.lang.LispReader$EvalReader invoke "LispReader.java" 1297]
+ ;;  [clojure.lang.LispReader$DispatchReader invoke "LispReader.java" 853]
+ ;;  [clojure.lang.LispReader read "LispReader.java" 285]
+ ;;  [clojure.lang.LispReader read "LispReader.java" 216]
+ ;;  [clojure.lang.LispReader read "LispReader.java" 205]
+ ;;  [clojure.lang.RT readString "RT.java" 1874]
+ ;;  [clojure.lang.RT readString "RT.java" 1869]
+ ;;  [clojure.core$read_string invokeStatic "core.clj" 3815]
+ ;;  [clojure.core$read_string invoke "core.clj" 3805]
+ ;;  [clips.core$eval13448 invokeStatic "NO_SOURCE_FILE" 55]
+ ;;  [clips.core$eval13448 invoke "NO_SOURCE_FILE" 55]
+ ;;  [clojure.lang.Compiler eval "Compiler.java" 7176]
+ ;;  [clojure.lang.Compiler eval "Compiler.java" 7131]
+ ;;  [clojure.core$eval invokeStatic "core.clj" 3214]
+ ;;  [clojure.core$eval invoke "core.clj" 3210]
+ ;;  [clojure.main$repl$read_eval_print__9068$fn__9071 invoke "main.clj" 414]
+ ;;  [clojure.main$repl$read_eval_print__9068 invoke "main.clj" 414]
+ ;;  [clojure.main$repl$fn__9077 invoke "main.clj" 435]
+ ;;  [clojure.main$repl invokeStatic "main.clj" 435]
+ ;;  [clojure.main$repl doInvoke "main.clj" 345]
+ ;;  [clojure.lang.RestFn applyTo "RestFn.java" 137]
+ ;;  [clojure.core$apply invokeStatic "core.clj" 665]
+ ;;  [clojure.core$apply invoke "core.clj" 660]
+ ;;  [refactor_nrepl.ns.slam.hound.regrow$wrap_clojure_repl$fn__9642 doInvoke "regrow.clj" 18]
+ ;;  [clojure.lang.RestFn invoke "RestFn.java" 1523]
+ ;;  [nrepl.middleware.interruptible_eval$evaluate invokeStatic "interruptible_eval.clj" 79]
+ ;;  [nrepl.middleware.interruptible_eval$evaluate invoke "interruptible_eval.clj" 55]
+ ;;  [nrepl.middleware.interruptible_eval$interruptible_eval$fn__935$fn__939 invoke "interruptible_eval.clj" 142]
+ ;;  [clojure.lang.AFn run "AFn.java" 22]
+ ;;  [nrepl.middleware.session$session_exec$main_loop__1036$fn__1040 invoke "session.clj" 171]
+ ;;  [nrepl.middleware.session$session_exec$main_loop__1036 invoke "session.clj" 170]
+ ;;  [clojure.lang.AFn run "AFn.java" 22]
+ ;;  [java.lang.Thread run "Thread.java" 748]]}
 
 (macroexpand `(-> 2 inc (* 2)));; => (clojure.core/* (clojure.core/inc 2) 2)
+
+;;;; Session 19/05/2019
+
+(let [{::keys [a]} #:my.namespace{:a 1}] (inc a))
+*e;; => #error {
+;;     :cause nil
+;;     :via
+;;     [{:type java.lang.NullPointerException
+;;       :message nil
+;;       :at [clojure.lang.Numbers ops "Numbers.java" 1068]}]
+;;     :trace
+;;     [[clojure.lang.Numbers ops "Numbers.java" 1068]
+;;      [clojure.lang.Numbers inc "Numbers.java" 137]
+;;      [clips.core$eval24439 invokeStatic "NO_SOURCE_FILE" 102]
+;;      [clips.core$eval24439 invoke "NO_SOURCE_FILE" 102]
+;;      [clojure.lang.Compiler eval "Compiler.java" 7176]
+;;      [clojure.lang.Compiler eval "Compiler.java" 7131]
+;;      [clojure.core$eval invokeStatic "core.clj" 3214]
+;;      [clojure.core$eval invoke "core.clj" 3210]
+;;      [clojure.main$repl$read_eval_print__9068$fn__9071 invoke "main.clj" 414]
+;;      [clojure.main$repl$read_eval_print__9068 invoke "main.clj" 414]
+;;      [clojure.main$repl$fn__9077 invoke "main.clj" 435]
+;;      [clojure.main$repl invokeStatic "main.clj" 435]
+;;      [clojure.main$repl doInvoke "main.clj" 345]
+;;      [clojure.lang.RestFn applyTo "RestFn.java" 137]
+;;      [clojure.core$apply invokeStatic "core.clj" 665]
+;;      [clojure.core$apply invoke "core.clj" 660]
+;;      [refactor_nrepl.ns.slam.hound.regrow$wrap_clojure_repl$fn__9698 doInvoke "regrow.clj" 18]
+;;      [clojure.lang.RestFn invoke "RestFn.java" 1523]
+;;      [nrepl.middleware.interruptible_eval$evaluate invokeStatic "interruptible_eval.clj" 79]
+;;      [nrepl.middleware.interruptible_eval$evaluate invoke "interruptible_eval.clj" 55]
+;;      [nrepl.middleware.interruptible_eval$interruptible_eval$fn__935$fn__939 invoke "interruptible_eval.clj" 142]
+;;      [clojure.lang.AFn run "AFn.java" 22]
+;;      [nrepl.middleware.session$session_exec$main_loop__1036$fn__1040 invoke "session.clj" 171]
+;;      [nrepl.middleware.session$session_exec$main_loop__1036 invoke "session.clj" 170]
+;;      [clojure.lang.AFn run "AFn.java" 22]
+;;      [java.lang.Thread run "Thread.java" 748]]}
+
+(let [{::keys [a]} #:clips.core{:a 1}] (inc 1)) ;; => 2
+
+(some identity [nil 1 nil 3]) ;; => 1
+
+() ;; => ()
+(type ()) ;; => clojure.lang.PersistentList$EmptyList
+;; It is not a fn call.. Weird!
+
+'() ;; => ()
+(type '()) ;; => clojure.lang.PersistentList$EmptyList
+
+
+;;;; Session 31/08/2019
+
+(defmacro if-let-xf
+  "Like `if-let` but it applies `xf` to the `if` reference symbol on branch 1.
+  It supports `:let` plus a vector like in a `for`."
+  [xf# [ref-symbol# assign-form# let?# extra-args#] & [branch1# branch2#]]
+  (let [orig-let-vec# (if (#{:let} let?#)
+                        (into [ref-symbol# assign-form#] extra-args#)
+                        [ref-symbol# assign-form#])]
+    `(let ~orig-let-vec#
+       (if ~ref-symbol#
+         (let [~ref-symbol# (~xf# ~ref-symbol#)]
+           ~branch1#)
+         ~branch2#))))
