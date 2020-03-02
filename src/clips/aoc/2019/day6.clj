6710de919c2b5dd2b76429fc09456beb29a2e748
Marc Sabates Gelpi <marc.sabates.gelpi@protonmail.com>
2020-03-02 19:40:25 +0000

    AoC 2019 Day 6 Part 2

--- src/clips/aoc/2019/day6.clj
+++ src/clips/aoc/2019/day6.clj
@@ -22,7 +22,6 @@
        string/split-lines
        (map #(string/split % #"\)"))
        (map (partial map (comp keyword string/lower-case)))
-       #_shuffle
        orbit-sort
        (reduce (fn [acc [orbited in-orbit]]
                  (when-not (get acc orbited) (throw (Exception. (format "[ %s ] Object %s hasn't been defined yet!" in-orbit orbited))))
@@ -38,3 +37,27 @@
 ;=> 42
 ;(part1 "resources/aoc2019/day6")
 ;=> 308790
+
+;;;; Part 2
+(defn part2 [s]
+  (let [orb-map (->> (if (string/starts-with? s "resources/")
+                       (slurp s)
+                       s)
+                     string/split-lines
+                     (map #(string/split % #"\)"))
+                     (map (partial map (comp keyword string/lower-case)))
+                     orbit-sort
+                     (reduce (fn [acc [orbited in-orbit]]
+                               (when-not (get acc orbited) (throw (Exception. (format "[ %s ] Object %s hasn't been defined yet!" in-orbit orbited))))
+                               (assoc acc in-orbit {:num  (inc (get-in acc [orbited :num]))
+                                                    :tree (conj (get-in acc [orbited :tree]) in-orbit)}))
+                             {:com {:num  0
+                                    :tree [:com]}}))
+        [san you :as paths] (map :tree (vals (select-keys orb-map [:san :you])))
+        common  (conj (set (filter (set san) you)) :you :san)]
+    (apply + (map (comp count (partial remove common)) paths))))
+
+;(part2 "COM)B\nB)C\nC)D\nD)E\nE)F\nB)G\nG)H\nD)I\nE)J\nJ)K\nK)L\nK)YOU\nI)SAN")
+;=> 4
+;(part2 "resources/aoc2019/day6")
+;=> 472
